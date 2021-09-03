e0756ad4fe21212d8fd7aaaee7a344a36f40c85b
201250067 <2384159848@qq.com>
2021-09-03 17:02:14 +0800

    updating

---
+++ 2021-数据结构与算法/上机课练习/1-2Hanoi/src/Hanoi.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Hanoi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the level of tower: ");
+        int N = input.nextInt();
+        hanoi(N, 'A', 'B', 'C');
+    }
+    private static void hanoi(int N, char ATower, char BTower, char CTower){
+        if (N == 1) {
+            System.out.println(ATower + " --> " + CTower);//每次移动中，ATower为起始塔，CTower为目标塔
+        }else {
+            //System.out.println(ATower + " --> " + BTower);
+            hanoi(N-1, ATower, CTower, BTower);
+            System.out.println(ATower + " --> " + CTower);
+            hanoi(N-1, BTower, ATower, CTower);
+        }
+    }
+}
