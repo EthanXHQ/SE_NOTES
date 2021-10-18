6d99feade220aa7708541e6ee60d49ac23c06859
201250067 <2384159848@qq.com>
2021-10-18 23:20:50 +0800

    U

--- 2021-数据结构与算法/上机课练习/1-1找出自然数/src/FindNature.java
+++ 2021-数据结构与算法/上机课练习/1-1找出自然数/src/FindNature.java
@@ -7,22 +7,21 @@ public class FindNature {
         int N = input.nextInt();
         System.out.print("Enter r: ");
         int r = input.nextInt();
-        if(r > N){
+        if (r > N) {
             System.out.print("Error!");
-        }else {
+        } else {
             int[] a = new int[r];
             function(N, r, a);
         }
     }
-    private static void function(int N, int r, int[] a){
-        for (int i = N; i >= r; i--)
-        {
-            a[r-1] = i;
+
+    private static void function(int N, int r, int[] a) {
+        for (int i = N; i >= r; i--) {
+            a[r - 1] = i;
             if (r > 1)
                 function(i - 1, r - 1, a);
-            else
-            {
-                for (int j = a.length-1; j >= 0; j--)
+            else {
+                for (int j = a.length - 1; j >= 0; j--)
                     System.out.print(a[j] + " ");
                 System.out.println();
             }
--- 2021-数据结构与算法/上机课练习/1-2Hanoi/src/Hanoi.java
+++ 2021-数据结构与算法/上机课练习/1-2Hanoi/src/Hanoi.java
@@ -7,14 +7,14 @@ public class Hanoi {
         int N = input.nextInt();
         hanoi(N, 'A', 'B', 'C');
     }
-    private static void hanoi(int N, char ATower, char BTower, char CTower){
+
+    private static void hanoi(int N, char ATower, char BTower, char CTower) {
         if (N == 1) {
-            System.out.println(ATower + " --> " + CTower);//每次移动中，ATower为起始塔，CTower为目标塔
-        }else {
-            //System.out.println(ATower + " --> " + BTower);
-            hanoi(N-1, ATower, CTower, BTower);
-            System.out.println(ATower + " --> " + CTower);
-            hanoi(N-1, BTower, ATower, CTower);
+            System.out.println(N +" "+ ATower + " --> " + CTower);//每次移动中，ATower为起始塔，CTower为目标塔
+        } else {
+            hanoi(N - 1, ATower, CTower, BTower);
+            System.out.println(N +" "+ ATower + " --> " + CTower);
+            hanoi(N - 1, BTower, ATower, CTower);
         }
     }
 }
